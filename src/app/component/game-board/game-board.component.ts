8ef9510a3736d31d022b1333559a85cfaf562d1a
Frederik von Berg <vonberg@w11k.de>
2017-02-16 16:38:10 +0100

    wip: removed commentted out code

--- src/app/component/game-board/game-board.component.ts
+++ src/app/component/game-board/game-board.component.ts
@@ -26,38 +26,4 @@ export class GameBoardComponent implements OnInit, OnDestroy {
   isFilled(cell: number) {
     return cell == 1 ? true: false;
   }
-  /*
-
-
-getFilletClass(cell) {
-  let pieceClass = '';
-  if (cell.filled) {
-    switch(cell.shape) {
-      case 0: pieceClass = 'dy-L-filled';
-        break;
-      case 1: pieceClass = 'dy-O-filled';
-        break;
-      case 2: pieceClass = 'dy-I-filled';
-        break;
-      case 3: pieceClass = 'dy-T-filled';
-        break;
-      case 4: pieceClass = 'dy-J-filled';
-        break;
-      case 5: pieceClass = 'dy-S-filled';
-        break;
-      case 6: pieceClass = 'dy-Z-filled';
-        break;
-      default: pieceClass = 'dy-X-filled';
-        break;
-    }
-  }
-  if (cell.ghost) {
-    pieceClass += (pieceClass.length > 0) ? ' dy-ghost-piece' : 'dy-ghost-piece';
-  }
-  if (cell.filled && cell.shape === 7) {
-      pieceClass += ' background-color:' + this.gameDate.getColor();
-  }
-  return pieceClass;
-}
-*/
 }
--- src/app/component/game/game.component.ts
+++ src/app/component/game/game.component.ts
@@ -9,96 +9,24 @@ import {GameService} from "../../service/game.service";
   styleUrls: ['./game.component.less']
 })
 export class GameComponent implements OnInit {
-  //private gameLoop: any;
 
   constructor(private gameService: GameService) {
   }
 
   ngOnInit() {
-    // this.gameLoop = this.setUpGameLoop();
     // this.keyboardService.init();
     this.newGame();
   }
-
-  // private setupGameLoop = () => {
-  //   return _.throttle(this.gameService.gameLoop, 600, {
-  //     leading: false,
-  //     trailing: false
-  //   });
-  // };
-
-  // private loop = this.setupGameLoop();
 
   private gameOn(timestamp: number) {
     window.requestAnimationFrame((timestamp) => {
       this.gameOn(timestamp);
     });
-    // this.loop();
     this.gameService.gameLoop(timestamp);
-//    if (!this.GameManager.isPause() && this.GameManager.isGameStart()) {
-//      this.gameLoop();
-//    }
   }
 
   private newGame() {
     this.gameService.newGame();
     this.gameOn(0)
   }
-
-  restartNewGame() {
-/*    this.GameManager.newGame();
-    this.GameManager.setGameStart();
-    window.scrollTo(0, 0);
-    return this;*/
-  }
-
-  getScore() {
-//    return this.GameManager.getScore();
-  }
-
-  startNewGame() {
-/*    this.GameManager.startNewGame();
-    this.gameOn(); */
-  }
-
-  isGameStart() {
-//    return this.GameManager.isGameStart();
-  }
-
-  isGameEnd() {
-//    return this.GameManager.isGameEnd();
-  }
-
-  isNewRecord() {
-//    return this.GameManager.getIsNewRecord();
-  }
-
-  getGameSpeed() {
-//    let curSpeed = this.GameManager.getGameSpeed();
-//    return GameSpeed.getGameSpeedTitle(curSpeed);
-  }
-
-  getBestScore() {
-    return "-";
-  }
-
-  setGameSpeed() {
-/*    if (this.isGameStart()) {
-      this.GameManager.setPause();
-    }*/
-  }
-
-  openDesignMenu() {
-//    this.GameManager.setOpenDesignBeforeStart(true);
-  }
-
-/*
-   $scope.$on('Piece.createNewPiece', function() {
-   this.GameManager.createNewPiece();
-   });
-
-   $scope.$on('BootstrapSlider.Speed', function() {
-   gameLoop = setUpGameLoop();
-   });
-   */
 }
