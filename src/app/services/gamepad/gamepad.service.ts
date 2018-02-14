679194178e3bf1916d539c9e3dd7668415564f12
Mark Kvetny <m.kvetny@outlook.com>
2018-02-14 22:13:27 +0100

    wip: implementing gamepad-service

--- src/app/services/gamepad/gamepad.service.ts
+++ src/app/services/gamepad/gamepad.service.ts
@@ -3,27 +3,39 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class GamepadService {
 
-  animationId: number;
+  gamepads: Gamepad[];
 
   constructor() {
     this.pollingLoop();
   }
 
-  stopPollingLoop() {
-    if (this.animationId) {
-      window.cancelAnimationFrame(this.animationId);
-      this.animationId = undefined;
-    }
-  }
-
   private pollingLoop() {
-    this.animationId = window.requestAnimationFrame(() => {
-      const gamepads = navigator.getGamepads();
-      if (gamepads[0] !== null || gamepads[1] !== null || gamepads[2] !== null) {
-        console.log('found it!');
+    window.requestAnimationFrame(() => {
+      this.gamepads = navigator.getGamepads();
+      for (let i = 0; i < 4; i++) {
+        const gamepad = this.gamepads[i];
+        if (gamepad === null) {
+          continue;
+        }
+        const buttons = gamepad.buttons
+          .reduce((acc, button, index) => {
+            if (button.pressed) {
+              acc.push(index);
+            }
+            return acc;
+          }, []);
+        const axes = gamepad.axes.slice(0, 2);
+        this.actionMapper(buttons, axes, i);
       }
       this.pollingLoop();
     });
+  }
+
+  private actionMapper(buttons: number[], axes: number[], index: number) {
+    // todo: implement
+    console.log(`buttons: ${buttons}`);
+    console.log(`axes: ${axes}`);
+    console.log(`index: ${index}`);
   }
 
 }
