889ba18ff4fcc7d7e560f431eb00ab8570ad9ad9
Can Kattw <kattwinkel@w11k.de>
2018-02-23 14:53:20 +0100

    feat(next-tetromino): adds logic to display the next tetromino

--- src/app/components/game-controller/tetromino/tetromino.component.ts
+++ src/app/components/game-controller/tetromino/tetromino.component.ts
@@ -1,6 +1,9 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
-import { Tetromino } from '../../../models/tetromino/tetromino.model';
-import { flatten } from '../../../helpers/tetromino-helpers';
+import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
+import {Tetromino, TetrominoType} from '../../../models/tetromino/tetromino.model';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import {Observable} from 'rxjs/Observable';
+import {map} from 'rxjs/operators';
+import {flatten} from '../../../helpers/tetromino-helpers';
 
 @Component({
   selector: 'app-tetromino',
@@ -10,16 +13,42 @@ import { flatten } from '../../../helpers/tetromino-helpers';
 })
 export class TetrominoComponent implements OnInit {
 
-  @Input() tetromino: Tetromino;
+  tetromino$ = new BehaviorSubject<Tetromino | null>(null);
+  coords$: Observable<string[]>;
+  gridWidth$: Observable<string>;
 
   constructor() {
+    this.coords$ = this.tetromino$.pipe(map(it => it ? flatten<number>(it.coordinates).map(num => num === 1 ? `active block-${it.type}` : '') : []));
+    this.gridWidth$ = this.tetromino$.pipe(map(it => this.typeToGrid(it.type)));
+  }
+
+  @Input()
+  set tetromino(value: Tetromino) {
+    this.tetromino$.next(value);
   }
 
   ngOnInit() {
   }
 
-  get coordinates(): number[] {
-    return flatten<number>(this.tetromino.coordinates);
+  typeToGrid(type: TetrominoType): 'grid-2' | 'grid-3' | 'grid-4' {
+    switch (type) {
+      case TetrominoType.I:
+        return 'grid-4';
+      case TetrominoType.J:
+        return 'grid-3';
+      case TetrominoType.L:
+        return 'grid-3';
+      case TetrominoType.O:
+        return 'grid-2';
+      case TetrominoType.S:
+        return 'grid-3';
+      case TetrominoType.T:
+        return 'grid-3';
+      case TetrominoType.Z:
+        return 'grid-3';
+      default:
+        throw new Error(`Wrong type '${type}' submitted.`);
+    }
   }
 
 }
