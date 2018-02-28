6a64901ce1255be425de40fa5c79a88161e8f0d7
mark <kvetny@w11k.de>
2018-02-28 17:57:05 +0100

    refactored a styles
    together with freddi <3

--- src/app/components/highscore-display/highscore-display.module.ts
+++ src/app/components/highscore-display/highscore-display.module.ts
@@ -4,13 +4,15 @@ import { HighscoreDisplayComponent } from './highscore-display.component';
 import { HeaderModule } from '../header/header.module';
 import { HighscoreModule } from '../../services/highscore/highscore.module';
 import { HighscoreListModule } from '../highscore-list/highscore-list.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
     HeaderModule,
     HighscoreModule,
-    HighscoreListModule
+    HighscoreListModule,
+    RouterModule
   ],
   declarations: [HighscoreDisplayComponent]
 })
