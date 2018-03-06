d0c80162d053479d3156ed720d4a5814906ccfe4
Can Kattw <kattwinkel@w11k.de>
2018-03-06 09:32:31 +0100

    This branch is so dirty, even your mom can't clean it.

--- src/app/routes/settings/auth/auth.component.ts
+++ src/app/routes/settings/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -10,9 +11,9 @@ export class AuthComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private router: Router) {
     this.form = this.formBuilder.group({
-      password: 'test',
+      password: '',
     });
   }
 
@@ -21,7 +22,8 @@ export class AuthComponent implements OnInit {
 
 
   login() {
-
+    if (this.form.get('password').value === 'ui-router') {
+      this.router.navigate(['/settings/internal']);
+    }
   }
-
 }
